dbb72682111546f6734a3f87e704f23a5f8fd457
Meliza Caguimbal <melizacaguimbal34@gmail.com>
2021-06-04 11:15:07 +0800

    add StatcImage using plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="person coding on laptop on table"
+        src="../images/code.jpg"
+      />
     </Layout>
   );
 };
